613d8f0416ef01906ffb3f6c62ca25f13143b4cd
Nandinikumari1 <154058355+Nandinikumari1@users.noreply.github.com>
2025-03-19 18:32:28 +0000

    continue se skip, 1 glti maaf

--- 05_iterations/one_i.js
+++ 05_iterations/one_i.js
@@ -1,8 +1,17 @@
 //break , continue
+// for (let index = 1; index <= 20 ; index++) {
+//     if(index == 5){
+//         console.log("detected 5");
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
 for (let index = 1; index <= 20 ; index++) {
     if(index == 5){
         console.log("detected 5");
-        break
+        continue                        //5 print nhi hoga wo glti maaf krke continue krega aage 
     }
     console.log(`value of i is ${index}`);
     
