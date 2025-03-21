23f28f41e0938369c9569ebc59961f508a3fe636
Nandinikumari1 <154058355+Nandinikumari1@users.noreply.github.com>
2025-03-21 17:07:59 +0000

    if

---
+++ 04_control _flow/01_one.js
@@ -0,0 +1,21 @@
+//if
+/*
+if (true) {
+    andar ka code execute hoga 
+}
+
+
+if (false) {
+    andar ka code execute nhi hoga kabbhi agr false h
+}
+
+*/
+
+const isLoggedIn = true
+const temperature = 41
+if(temperature < 50)
+{
+    console.log("less than 50");
+    
+}
+console.log("temp is greater than 50");
