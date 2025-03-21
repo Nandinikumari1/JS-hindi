cda47c440c8c31b495b0bf771530653071e5e8d5
Nandinikumari1 <154058355+Nandinikumari1@users.noreply.github.com>
2025-03-21 17:47:23 +0000

    switch

--- 04_control _flow/01_one.js
+++ 04_control _flow/01_one.js
@@ -1,3 +1,4 @@
+//
 //if
 /*
 if (true) {
@@ -19,3 +20,31 @@ if(temperature < 50)
     
 }
 console.log("temp is greater than 50");
+
+//implicit if, bina bracket diye likhna smae line m
+const balance = 1000
+if (balance> 500) console.log("test1"), console.log("test44444");
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("use valid key");
+        
+        break;
+}
